dea2683f3ab37e84c543dc73d3a3c855651dcfe9
azulmanwh <20010434@woodhouse.ac.uk>
2021-09-30 21:11:00 +0100

    small optimisations

--- main.py
+++ main.py
@@ -2,19 +2,16 @@ from perlin_noise import PerlinNoise
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
-res = 60
+res = 100
 
-class Block(Button):
+class Block(Entity):
     def __init__(self, position = (0,0,0)):
         super().__init__(
-            parent = scene,
             position = position,
             model = 'cube',
             origin_y = 0.5,
             texture = './Textures/stone.png',
-            color = color.gray,
-            highlight_color = color.light_gray,
-            collider = 'box'
+            color = color.gray
             )
 
 def whatChunkAmIIn():
@@ -23,16 +20,14 @@ def whatChunkAmIIn():
     z = round((player.z-5) / 10)
     return (x,z)
 
-def createChunk(coordinates, loadedChunks, playerCoordinates):
+def createChunk(coordinates, loadedChunks):
     if(coordinates not in set(loadedChunks)):
-        print(loadedChunks)
+        pixelX = coordinates[0]*10
+        pixelY = coordinates[1]*10
         for z in range(10):
             for x in range(10):
-                y = noise([((coordinates[0]*10) + x)/res,((coordinates[1]*10) + z)/res])
-                block = Block(position = ((coordinates[0]*10) + x,y*3,(coordinates[1]*10) + z))
+                Block(position = (pixelX + x, round(noise([(pixelX + x)/res, (pixelY + z)/res])*3), pixelY + z))
         loadedChunks.append(coordinates)
-    else:
-        return
 
 def handle_movement():
     #time.dt is the difference between a second and the frequency of the game being run so that the game speed is the same regardless of 
@@ -43,10 +38,7 @@ def handle_movement():
         player.y += 3 * time.dt
 
 def toggleGravity():
-    if(player.gravity == 0):
-        player.gravity = 1
-    else:
-        player.gravity = 0
+    player.gravity = (1 if player.gravity == 0 else 0)
 
 def input(key):
     if(key == 'escape'):
@@ -58,20 +50,17 @@ def input(key):
 
 app = Ursina()
 
-noise = PerlinNoise(octaves = 10)
+noise = PerlinNoise(octaves = 16)
 
 #This creates the first chunk of the game.
 
 player = FirstPersonController(gravity=0)
 loadedChunks = []
-createChunk((0,0), loadedChunks, (0,0))
-
-chunkCoordinates = Text()
+createChunk((0,0), loadedChunks)
 
 def update():
     currentChunk = whatChunkAmIIn()
-    chunkCoordinates.text = str(currentChunk[0]) + str(currentChunk[1])
-    createChunk(currentChunk, loadedChunks, (player.x, player.z))
+    createChunk(currentChunk, loadedChunks)
     #Here another chunk will be created based on what chunk the player is in
     #but a check needs to be made for wether the chunk has already been loaded.
     #Maybe this value can be held in a list of tuples for loaded chunks.

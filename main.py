cab17c4df443c58cb9f68f7b1c3e6ddc1b410f2e
azulmanwh <70888545+azulmanwh@users.noreply.github.com>
2021-12-08 12:15:04 +0000

    Updated ChunkGenerator
    Added load function with limited functionality, fixed reading and writing to save file.

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from abc import ABCMeta, abstractmethod
 TO DO LIST:
 -implement observer for chunk changing
 -when creating a chunk, first try to load the chunk, if you can't find the chunk in the world file, then create the chunk.
--get a mountain to render (mess around with the perlin noise generation)
 -create chunks on a separate thread for performance
+-create chunks with multiply layers of blocks, not just a top layer
 """
 class ChunkChecker():
 
@@ -20,9 +20,9 @@ class ChunkChecker():
         file = open(self.__path, 'r')
         data = file.readlines()
         for line in data:
-            x = line.split("|")[0].split(",")[0] #returns the x coordinate of a bit of chunk data from file
-            y = line.split("|")[0].split(",")[1] #returns the y coordinate
-            if((chunkCoords.getX() == x) and (chunkCoords.getY() == y)):
+            x = line.split(";")[0].split(",")[0] #returns the x coordinate of a bit of chunk data from file
+            y = line.split(";")[0].split(",")[1] #returns the y coordinate
+            if((chunkCoords.getX() == int(x)) and (chunkCoords.getY() == int(y))):
                 return True
         return False
 
@@ -38,11 +38,11 @@ class ChunkLocator():
         y = round((coords.getZ()-5) / 10)
         return ChunkCoords(x, y)
 
-    def getData(chunkCoords):
+    def getData(self, chunkCoords):
         file = open("ChunkData.txt", 'r')
         data = file.readlines()
         for line in data:
-            chunkData = line.split("|")[1]
+            chunkData = line.split(";")[1]
             return chunkData
 
 class ChunkSaver():
@@ -52,43 +52,60 @@ class ChunkSaver():
         self.__locator = locator
 
     def saveChunkData(self, chunkCoords, data):
+        """
+        Chunk data is stored as the chunk coordinates of the chunk and then the y values of each block
+        stored as comma separated values. The chunk coordinates in the beginning are comma separated as well
+        the chunk coordinates and block coordinates are separated by a semicolon.
+
+        Example-->
+        -1,1;2,2,2,2,2,2,1,1,1,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,0,0,0,0,1,1,2,1,1,1,0,0,0,0,1,2,2,2,1,1,0,0,0,1,2,2,3,2,2,1,0,-1
+        
+        """
         yCoordinates = data
         file = open(self.__path, 'a')
         try:
-            line = str(chunkCoords.getX()) + "," + str(chunkCoords.getY()) + "| "
+            line = str(chunkCoords.getX()) + "," + str(chunkCoords.getY()) + ";"
             for coordinate in yCoordinates:
-                line += " " + str(coordinate)
+                line += str(coordinate) + ","
             file.write(line + "\n")
         finally:
             file.close()
 
 class ChunkGenerator():
 
-    def __init__(self, checker, noise, saver):
+    def __init__(self, checker, noise, saver, locator):
         self.__checker = checker
         self.__noise = noise
+        self.__locator = locator
         self.saver = saver
 
     def generate(self, coordinates):
-        ycoordinates = []
-        chunkData = ChunkData(coordinates)
-        #pixel will be the coordinate of the noise for the block, which needs to be adjusted as the coordinates are chunk coordinates
-        pixelX = coordinates.getX()*10
-        pixelY = coordinates.getY()*10
-        heightScale = 20 #how high the blocks will spawn multiplier
-        res = 200 #"width" of the noisemap
-        for z in range(10):
-            for x in range(10):
-                #instantiates a block at the location
-                #the noise refers to a float which will be the height of the block
-                #saving to chunkData allows for the blocks to be changed after being instantiated
-                #saving to ycoordinates so that the chunkdata can be saved
+        if(self.__checker.hasBeenGenerated(coordinates)):
+            self.load(coordinates)
+        else:
+            ycoordinates = []
+            chunkData = ChunkData(coordinates)
+            #pixel will be the coordinate of the noise for the block, which needs to be adjusted as the coordinates are chunk coordinates
+            pixelX = coordinates.getX()*10
+            pixelY = coordinates.getY()*10
+            heightScale = 20 #how high the blocks will spawn multiplier
+            res = 200 #"width" of the noisemap
+            for z in range(10):
+                for x in range(10):
+                    #instantiates a block at the location
+                    #the noise refers to a float which will be the height of the block
+                    #saving to chunkData allows for the blocks to be changed after being instantiated
+                    #saving to ycoordinates so that the chunkdata can be saved
+                    y_value = round(self.__noise([(pixelX + x)/res, (pixelY + z)/res])*heightScale)
+                    block = Block(position = (pixelX + x, y_value, pixelY + z))
+                    chunkData.addBlock(block)
+                    ycoordinates.append(y_value)
+            self.saver.saveChunkData(coordinates, ycoordinates)
+            return chunkData
 
-                block = Block(position = (pixelX + x, round(self.__noise([(pixelX + x)/res, (pixelY + z)/res])*heightScale), pixelY + z))
-                chunkData.addBlock(block)
-                ycoordinates.append(round(self.__noise([(pixelX + x)/res, (pixelY + z)/res])*3))
-        self.saver.saveChunkData(coordinates, ycoordinates)
-        return chunkData
+    def load(self, coordinates):
+        y_coordinates = self.__locator.getData(coordinates)
+        print(y_coordinates)
 
 class Block(Entity):
 
@@ -179,7 +196,7 @@ class Game():
         self.noise = noise
         self.locator = ChunkLocator(player, "ChunkData.txt")
         self.movementHandler = MovementHandler(ChunkLocator(player, "ChunkData.txt")) # Initiates movement handler.
-        self.generator = ChunkGenerator(ChunkChecker("ChunkData.txt"), noise, ChunkSaver("ChunkData.txt", self.locator))
+        self.generator = ChunkGenerator(ChunkChecker("ChunkData.txt"), noise, ChunkSaver("ChunkData.txt", self.locator), self.locator)
         self.chunkData = []
 
     def start(self):
